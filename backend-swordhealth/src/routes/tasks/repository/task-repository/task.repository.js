3a5b85be7d6c9df4a6c4632b02888d96d74194ee
Alexandre Ferrreira <lexanjos@icloud.com>
2022-08-27 20:20:42 +0100

    rename column

--- backend-swordhealth/src/routes/tasks/repository/task-repository/task.repository.js
+++ backend-swordhealth/src/routes/tasks/repository/task-repository/task.repository.js
@@ -23,7 +23,7 @@ class TaskRepository {
     const { connection } = await this.database.getConnection();
     try {
       const query = connection
-        .select("id", "user_id", "name", "summary", "perfomed_task")
+        .select("id", "user_id", "name", "summary", "perfomed_task_date")
         .from("tasks");
       if (user.role === "technician") query.where("user_id", user.id);
       query.limit(limit).offset(offset);
@@ -46,7 +46,7 @@ class TaskRepository {
     const { connection } = await this.database.getConnection();
     try {
       return await connection
-        .select("id", "user_id", "name", "summary", "perfomed_task")
+        .select("id", "user_id", "name", "summary", "perfomed_task_date")
         .from("tasks")
         .where("id", taskId)
         .andWhere("user_id", userId)
